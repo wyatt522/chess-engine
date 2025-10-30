8701df1d5868e82c7fd2296f0d60b98f20bbc573
wyatt522 <harrisw522@gmail.com>
2025-10-29 20:24:12 -0400

    reduced memory consumption for dataset generation

--- engines/torch/auxiliary_func.py
+++ engines/torch/auxiliary_func.py
@@ -1,8 +1,9 @@
 import numpy as np
-from chess import Board
+from chess import Board, pgn
 from tqdm import tqdm # type: ignore
 import psutil
 from collections import deque
+import random
 
 
 def board_to_matrix(board: Board):
@@ -34,34 +35,55 @@ def check_memory():
     available_gb = mem.available / (1024 ** 3)
     return available_gb
 
+def load_pgn(file_path, pgn_memory_mark = 1.0):
+    games = []
+    with open(file_path, 'r') as pgn_file:
+        # print(file_path)
+        i = 0
+        while True:
+            game = pgn.read_game(pgn_file)
+            if game is None:
+                break
+            yield game
 
-
-def create_input_for_nn(games):
-
-    LOW_MEMORY_GB = 5.0
-
+def create_input_for_nn(game, move_collection_prob = 0.1):
     X = []
     y = []
 
-    for i, game in enumerate(tqdm(games)):
+    board = game.board()
+    for move in game.mainline_moves():
 
-        # Check memory every 100 games
-        if i % 100 == 0:
-            available_gb = check_memory()
-            
-            if available_gb < LOW_MEMORY_GB:
-                print(f"Low memory hit at {i} games")
-                print(available_gb)
-                break
-
-        board = game.board()
-        for move in game.mainline_moves():
+        if random.random() < move_collection_prob:
             X.append(board_to_matrix(board))
             y.append(move.uci())
-            board.push(move)
-    return np.array(X, dtype=np.float32), np.array(y)
+
+        board.push(move)
+    return X, y
 
 
 def encode_moves(moves):
     move_to_int = {move: idx for idx, move in enumerate(set(moves))}
     return np.array([move_to_int[move] for move in moves], dtype=np.float32), move_to_int
+
+def load_dataset(files, pgn_memory_mark = 3.0, file_limit = 30):
+    X, y = [], []
+    games_parsed = files_parsed = 0
+
+    for file in tqdm(files):
+
+        for game in load_pgn(f"../../data/{file}"):
+            games_parsed += 1
+            x_temp, y_temp = create_input_for_nn(game)
+            X.extend(x_temp)
+            y.extend(y_temp)
+
+            if games_parsed % 100 == 0:
+                available_gb = check_memory()
+                if available_gb < pgn_memory_mark:
+                    print(f"Completed sampling {files_parsed} files with {available_gb} remaining")
+                    return X, y
+
+        files_parsed += 1
+        if files_parsed >= file_limit:
+            print(f"Completed sampling limit of files with {available_gb} remaining")
+            return X, y
