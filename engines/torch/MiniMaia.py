2f499ba7e5b4f63673d4d9fd55cc0e2739d8dc9b
wyatt522 <harrisw522@gmail.com>
2025-11-18 21:03:01 -0500

    endtable prediction model, and seperated maia skip, skip fc, and regular

--- engines/torch/MiniMaia.py
+++ engines/torch/MiniMaia.py
@@ -50,6 +50,45 @@ class MiniMaia(nn.Module):
         super(MiniMaia, self).__init__()
         # Declare Layers and Blocks
         self.init_layer = nn.Conv2d(16, 64, kernel_size=3, padding=1)
+        self.final_layer = nn.Conv2d(64, 80, kernel_size=3, padding=1)
+        self.init_bn = nn.BatchNorm2d(64)
+        self.final_bn = nn.BatchNorm2d(80)
+        self.fc1 = nn.Linear(64 * 80, squeeze_layer)
+        self.fc2 = nn.Linear(squeeze_layer, num_classes)
+        
+        self.mid_layers_list = []
+        for i in range(num_blocks):
+            self.mid_layers_list.append(MiniMaiaBlock())
+
+        self.mid_layers = nn.Sequential(*self.mid_layers_list)
+
+        # Needed transformations
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+
+        # Initialize weights
+        nn.init.kaiming_uniform_(self.init_layer.weight, nonlinearity='relu')
+        nn.init.kaiming_uniform_(self.final_layer.weight, nonlinearity='relu')
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+
+    def forward(self, x):
+        x = self.relu(self.init_bn(self.init_layer(x)))
+
+        x = self.mid_layers(x)
+        
+        x = self.relu(self.final_bn(self.final_layer(x)))
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)  # Output raw logits
+        return x
+
+# Block based squeeze and excite net
+class MiniMaiaSkip(nn.Module):
+    def __init__(self, num_classes, num_blocks = 6, squeeze_layer = 1024):
+        super(MiniMaiaSkip, self).__init__()
+        # Declare Layers and Blocks
+        self.init_layer = nn.Conv2d(16, 64, kernel_size=3, padding=1)
         self.final_layer = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.init_bn = nn.BatchNorm2d(64)
         self.final_bn = nn.BatchNorm2d(64)
@@ -86,9 +125,9 @@ class MiniMaia(nn.Module):
 
 
 # Block based squeeze and excite net without fully connected final layers
-class MiniMaiaFC(nn.Module):
+class MiniMaiaSkipFC(nn.Module):
     def __init__(self, num_classes, num_blocks = 6):
-        super(MiniMaiaFC, self).__init__()
+        super(MiniMaiaSkipFC, self).__init__()
         # Declare Layers and Blocks
         self.init_layer = nn.Conv2d(16, 64, kernel_size=3, padding=1)
         self.final_layer = nn.Conv2d(64, 64, kernel_size=3, padding=1)
